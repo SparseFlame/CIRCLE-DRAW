dda77e95c37192ad57d811cf37326c27cfe9a4bc
SparseFlame <84712454+SparseFlame@users.noreply.github.com>
2021-11-27 23:13:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+var mouseEvent = "empty";
+var lx , ly;
+cavas = document.getElementById('myCanvas');
+ctx = canvas.getContext("2d");
+color = "blue";
+width = 10;
+canvas.addEventListener("mouseDown", functionname);
+function functionname(e){
+    mouseEvent = "mouseDown";
+}
+
+canvas.addEventListener("mouseMove", functionname2);
+function functionname2(e){
+    curposmox = e.clientX - canvas.offsetLeft;
+    curposmoy = e.clientY - canvas.offsetTop;
+
+    if (mouseEvent = "mouseDown") {
+    
+        ctx.beginPath();
+        ctx.strokeStyle = color;
+        ctx.lineWidth = width;
+        console.log("Last Position of X and Y Cordinates = ");
+        console.log("X = " + lx + ",Y = " + ly);
+        ctx.moveTo(lx, ly);
+        console.log("Current Position of X and Y Cordinates = ");
+        console.log("X = " + curposmox + ",Y = " + curposmoy);
+        ctx.arc(200, 200, 40, 0, 2*Math.PI);
+        ctx.stroke();
+    }
+}
+
+canvas.addEventListener("mouseUp", mup);
+
+function mup(e){
+    mouseEvent = "mouseUp";
+}
+
+canvas.addEventListener("mouseLeave", mle);
+
+function mle(e){
+    mouseEvent = "mouseLeave";
+}
